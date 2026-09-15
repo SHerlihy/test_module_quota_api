4126521d8a5ce3d25f6dd7ce8e149098ec9bd174
Steven Herlihy <steven_herlihy@yahoo.com>
2026-09-15 12:24:49 +0100

    format

--- test/output.tf
+++ test/output.tf
@@ -1,9 +1,9 @@
 output "endpoint_list" {
   value = [for path_config in local.path_configs :
-  {
-        endpoint : "${module.deploy_apis[path_config.api_name].endpoint}/${path_config.route_path}",
-        method : path_config.http_method,
-        api_key : module.deploy_apis[path_config.api_name].api_key
-      }
+    {
+      endpoint : "${module.deploy_apis[path_config.api_name].endpoint}/${path_config.route_path}",
+      method : path_config.http_method,
+      api_key : module.deploy_apis[path_config.api_name].api_key
+    }
   ]
 }
